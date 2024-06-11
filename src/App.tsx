e9f750783e370c18f42d9ea743356e46771bffbf
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2024-06-10 21:40:16 -0300

    :recycle: refactor: Refatorando código do arquivo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { FoodList } from "./components/food-list";
 import { Header } from "./components/header";
 
 export function App() {
   return (
     <div className="max-w-[1448px] mx-auto px-4 py-3 flex flex-col gap-5">
-      <Header />
-      <FoodList />
+      <Router>
+        <Header />
+        <Routes>
+        <Route path="/foods/:category" element={<FoodList />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
